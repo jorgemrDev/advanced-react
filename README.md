22c213409ff19c4d7c250e6a0e9bff5270549a9a
jorge <black.spade.666@gmail.com>
2021-05-11 11:47:48 -0500

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# advanced-react
+# Advanced React.js
+
+For help, ask in #questions at [slack.jscomplete.com](http://slack.jscomplete.com/)
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,43 @@
-const path = require("path");
+const path = require('path');
+const webpack = require('webpack');
 
-module.exports = {
+const config = {
   resolve: {
-    modules: [path.resolve("./lib"), path.resolve("./node_modules")],
+    modules: [path.resolve('./lib'), path.resolve('./node_modules')]
   },
-  entry: ["babel-polyfill", "./lib/renderers/dom.js"],
+  entry: {
+    vendor: [
+      'babel-polyfill',
+      'react',
+      'react-dom',
+      'prop-types',
+      'axios',
+      'lodash.debounce',
+      'lodash.pickby'
+    ],
+    app: ['./lib/renderers/dom.js']
+  },
   output: {
-    path: path.resolve(__dirname, "public"),
-    filename: "bundle.js",
+    path: path.resolve(__dirname, 'public'),
+    filename: '[name].js'
   },
   module: {
-    rules: [{ test: /\.js$/, exclude: /node_modules/, use: "babel-loader" }],
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['react', 'env', 'stage-2']
+        }
+      }
+    }]
   },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor'
+    })
+  ]
 };
+
+module.exports = config;
